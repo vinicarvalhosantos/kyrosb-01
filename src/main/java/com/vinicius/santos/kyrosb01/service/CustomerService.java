59b028226c0eaf125f289c2fffd07084b3eb2f42
Vinicius Santos <vinicius.santos@ernetwork.com.br>
2020-11-20 18:54:52 -0300

    Removed unused imports

--- src/main/java/com/vinicius/santos/kyrosb01/service/CustomerService.java
+++ src/main/java/com/vinicius/santos/kyrosb01/service/CustomerService.java
@@ -7,7 +7,6 @@ import com.vinicius.santos.kyrosb01.response.RequestResponse;
 import com.vinicius.santos.kyrosb01.response.SuccessResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
